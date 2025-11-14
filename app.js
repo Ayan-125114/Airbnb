4cf072d0f86bae39af39d5c85ad7e563acdabcb2
Muhammad Ayan <muhammad125114@gmail.com>
2025-11-14 17:54:16 +0530

    update image

--- app.js
+++ app.js
@@ -77,7 +77,6 @@ app.get("/listings/:id/edit" , async(req , res)=>{
 app.put("/listings/:id" , async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id , {...req.body.listing});
-    await Listing.findByIdAndUpdate(id , req.body.listing.url);
     res.redirect("/listings");
 
 });
--- models/listing.js
+++ models/listing.js
@@ -13,10 +13,10 @@ const listingSchema = new Schema({
         },
         url : {
         type : String,
-        default : "https://unsplash.com/photos/mountain-peak-reflecting-in-calm-lake-at-sunrise-FLfNL6XuOEM",
+        // default : "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
         set : (v) =>
             v ==="" 
-            ? "https://unsplash.com/photos/mountain-peak-reflecting-in-calm-lake-at-sunrise-FLfNL6XuOEM"
+            ? "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
             :v,
         }       
        
